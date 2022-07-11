ecc4a9167e6e0f53d537e12638738f1ced4bde28
sungho8203 <sungho8203@gmail.com>
2022-07-11 18:53:46 +0900

    always save

---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private bool run = false;
+        private void Run()
+        {
+            while(true)
+            {
+                if (run)
+                    SendKeys.SendWait("^(s)");
+                Thread.Sleep(5000);
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            Thread thread = new Thread(Run);
+            thread.IsBackground = true;
+            thread.Start();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            run = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            run = true;
+        }
+    }
+}
